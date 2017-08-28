97402f063e7fdde2f571adbee91287c5e851a5d7
Benjamin Silfen <bfsilfen@gmail.com>
2017-08-28 16:27:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 function add(){
+  var = a
+  var = b  
   return a+b
 }
+
 function subtract(){
+  var = a
+  var = b
   return a-b
 }
+
 function multiply(){
+  var = a
+  var = b 
   return a*b
 }
+
 function divide(){
+  var = a
+  var = b 
   return a/b
 }
