9b2ca200775201be3aa178d4ab64e46017233968
Benjamin Silfen <bfsilfen@gmail.com>
2017-08-28 19:37:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,5 @@ function makeInt(a){
   return parseInt(n,10);
 }
 function preserveDecimal(n){
-  return parseFloat('2.222');
-}
-function preserveDecimal(a){
-  return parseInt('sldkjflksjf',10);
+  return parseFloat(n);
 }
